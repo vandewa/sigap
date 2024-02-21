74e671ec1a43ac69eac5d19388ad2240c199c8df
vandewa <devandewananta@gmail.com>
2024-02-21 21:36:42 +0700

    temporary directory

--- config/livewire.php
+++ config/livewire.php
@@ -68,7 +68,7 @@ return [
         // Example: 'local', 's3'              | Default: 'default'
         'rules' => null,
         // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
-        'directory' => null,
+        'directory' => 'sigap/livewire-tmp',
         // Example: 'tmp'                      | Default: 'livewire-tmp'
         'middleware' => null,
         // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
